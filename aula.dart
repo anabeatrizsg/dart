a41580f3bba3e39d674be412c859141ab574d83f
ANA BEATRIZ SILVA GONCALVES <goncalves.5970@aluno.pr.senac.br>
2024-05-02 09:56:41 -0300

    first commit

---
+++ aula.dart
@@ -0,0 +1,14 @@
+void main(){
+  int nota1 = 6;
+  int nota2 = 7;
+  int nota3 = 8;
+  int nota4 = 5;
+
+ double media = (nota1 + nota2 + nota3 + nota4) /4;
+ 
+ if (media  >=7) {
+  print("voce foi aprovado!");
+ } else if (media >= 4 && media < 7) {
+  print("voce está em exame!");
+  } else {
+  } 
